ac218d959ece85a9093ce104f1a132ea0673a1df
VitalieVSV <dut66AHJCT>
2020-03-14 19:41:58 +0200

    modified

--- js/visual.js
+++ js/visual.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", () =>{
     console.log(buttonsList);
     for (let i = 0; i < buttonsList.length; i++){
         buttonsList[i].addEventListener('click', () =>{
-            buttonsList[i].style.backgroundColor = 'white';
-            buttonsList[i].style.color = 'black';
-            buttonsList[i].style.border = '1px solid red';
+            buttonsList[i].style.color = 'green';
+            buttonsList[i].innerHTML = 'Added <i class="fas fa-check fa-1x"></i>';
+            buttonsList[i].style.border = 'none';
         });
     }
 
